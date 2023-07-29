72ef741c738bf91f27bdde6b358141170e1b55d2
Ajay Pradhan <azaypdn@gmail.com>
2023-07-29 23:50:36 +0530

    feat: typeorm/db configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as path from 'node:path';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(
-    {
-      isGlobal : true,
-      envFilePath : path.join(process.cwd() , `${process.env?.NODE_ENV || 'development'}.env`),
-    }
-  )],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.join(
+        process.cwd(),
+        `${process.env?.NODE_ENV || 'development'}.env`,
+      ),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        username: configService.get('DB_USER'),
+        port: configService.get('DB_PORT'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: ['dist/**/*.entity.js'],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
